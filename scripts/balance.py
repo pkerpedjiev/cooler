f7b0f3850718a9ad0a2c12076a3751b100f0d743
Nezar <nabdennur@gmail.com>
2016-08-10 21:35:16 -0400

    Add arg parser to balancing script

--- scripts/balance.py
+++ scripts/balance.py
@@ -11,12 +11,17 @@ import cooler.ice
 N_CPUS = 5
 
 if __name__ == '__main__':
-    # Compute a genome-wide balancing/bias/normalization vector
-    # *** assumes uniform binning ***
+    parser = argparse.ArgumentParser(
+        description="Compute a genome-wide balancing/bias/normalization vector. Assumes uniform binning.")
+    parser.add_argument(
+        "cooler_file",
+        help="Cooler file",
+        metavar="COOLER_PATH")
+    args = vars(parser.parse_args())
     chunksize = int(100e6)
     try:
         pool = Pool(N_CPUS)
-        with h5py.File(COOLER_PATH, 'a') as h5:
+        with h5py.File(args['cooler_file'], 'a') as h5:
             bias = cooler.ice.iterative_correction(
                 h5, chunksize=chunksize, tol=1e-05, min_nnz=100,
                 cis_only=False, ignore_diags=3, map=pool.map)
